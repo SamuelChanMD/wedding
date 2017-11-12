0a0f175bbf68fa4b4a6750d7594ab8b7cb48dbd6
SamuelChanMD <samuelchanmd@gmail.com>
2017-11-12 17:54:43 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'Wedding 3')
+
+@section('content')
+	<h1>Testing</h1>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<title>@yield('title')</title> 
+
+<!--Meta Tags-->
+<meta charset='utf-8'>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--CSS-->
+<link href='{!! asset('/bootstrap/css/bootstrap.min.css')!!}' rel='stylesheet'>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
