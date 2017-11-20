608d0d8f2d88a8f7284873485a51d1066cf7ea88
Paul Loh <ploh007@uottawa.ca>
2017-11-20 18:58:33 -0500

    Put RSVP form into modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layout') @section('title', 'Sam & Sarah') @section('content')
-<div id='bannerImage' style='background-image: url(" {!! ( isset($img_banner) ? asset($img_banner) : asset( 'images/home_page.JPG')) !!} ");'>
+<div id='bannerImage' style='background-image: url(" {!! ( isset($img_banner) ? asset($img_banner) : asset( "images/home_page.JPG")) !!} ");'>
     <div class='caption'>
         <div class='container'>
             <div class='row'>
@@ -21,7 +21,6 @@
 </div>
 <div class='jumbotron test' id='third-body'>
     <div id='carousel-section' class='carousel slide' data-ride='carousel'>
-        
         <div class='carousel-inner text-center' role='listbox'>
             <div class='item active'>
                 <div class='container' id='details-section'>
@@ -30,7 +29,7 @@
                     <h2>StoneFields</h2>
                     <h2>1985 9th Line Carleton Place  ON K7C 3P2</h2>
                     <h2>1500 HR till 2300 HR</h2>
-                    <div id='rsvp-button'><a href='{{route("rsvp")}}'> RVSP </a></div>
+                    <div id='rsvp-button'><a href='{{route("rsvp")}}'>RSVP</a></div>
                 </div>
                 <div class='carousel-caption'>
                 </div>
@@ -42,7 +41,9 @@
                     <h2>StoneFields</h2>
                     <h2>1985 9th Line Carleton Place  ON K7C 3P2</h2>
                     <h2>1500 HR till 2300 HR</h2>
-                    <div id='rsvp-button'>RVSP</div>
+                    <button type="button" class="btn btn-default btn-lg" id='rsvp-button' data-toggle="modal" data-target="#myModal">
+                        RVSP
+                    </button>
                 </div>
                 <div class='carousel-caption'>
                 </div>
@@ -79,5 +80,125 @@
 </div>
 <div class='container text-center'>
     <footer> Sam and Sarah 2017 </footer>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                <div class='panel-body' id='rsvp-section'>
+                    <h1 class='text-center'>RSVP</h1>
+                    <form class='form-horizontal'>
+                        <div class="form-group row">
+                            <label for="firstName" class="col-sm-6 control-label">First Name</label>
+                            <div class="col-sm-4">
+                                <input type="textbox" class="form-control" id="firstName" name="firstName" placeholder="First Name">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="lastName" class="col-sm-6 control-label">Last Name</label>
+                            <div class="col-sm-4">
+                                <input type="textbox" class="form-control" id="lastName" name="lastName" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="attending" class="col-sm-6 control-label">Attending?</label>
+                            <div class="col-sm-4">
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="attending" id="attending1" value="yes"> Yes!
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="attending" id="attending2" value="no"> No (sadface)
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='hiddenUntilAttending'>
+                            <div class="form-group">
+                                <label for="isKid" class="col-sm-6 control-label">Are you under the age of 12?</label>
+                                <div class="col-sm-4">
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="isKid" id="isKid1" value="yes"> Straight outta the womb
+                                        </label>
+                                    </div>
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="isKid" id="isKid2" value="no"> No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='hiddenUntilChild'>
+                            <div class="form-group row">
+                                <label for="parentFirstName" class="col-sm-6 control-label"> Parent First Name</label>
+                                <div class="col-sm-4">
+                                    <input type="textbox" class="form-control" id="parentFirstName" name="parentFirstName" placeholder="Parent First Name">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="parentLastName" class="col-sm-6 control-label">Parent Last Name</label>
+                                <div class="col-sm-4">
+                                    <input type="textbox" class="form-control" id="parentLastName" name="parentLastName" placeholder="Parent Last Name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class='hiddenUntilNotChild'>
+                            <div class="form-group">
+                                <label for="receivedDirectInvitation" class="col-sm-6 control-label">Did you receive a direct invitation?</label>
+                                <div class="col-sm-4">
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="receivedDirectInvitation" id="receivedDirectInvitation1" value="yes"> Yes
+                                        </label>
+                                    </div>
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="receivedDirectInvitation" id="receivedDirectInvitation2" value="no"> No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='hiddenUntilNotDirectInvitation'>
+                            <div class="form-group row">
+                                <label for="invitorFirstName" class="col-sm-6 control-label">Who are you going with?</label>
+                                <div class="col-sm-4">
+                                    <input type="textbox" class="form-control" id="invitorFirstName" name="invitorFirstName" placeholder="Invitor First Name">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="invitorLastName" class="col-sm-6 control-label">Who are you going with?</label>
+                                <div class="col-sm-4">
+                                    <input type="textbox" class="form-control" id="invitorLastName" name="invitorLastName" placeholder="Invitor Last Name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class='hiddenUntilAttending'>
+                            <p>Want a reminder? Fill out your electronic-mail below and we will give you an update a month beforehand!</p>
+                            <div class='form-group'>
+                                <label for="email" class="col-sm-6 control-label">E-Mail</label>
+                                <div class="col-sm-4">
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="E-Mail">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
 </div>
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,6 +22,51 @@
 <script type='text/javascript' src='{!! asset("/js/jquery-1.9.1.min.js")!!}'></script>
 <script type='text/javascript' src='{!! asset("/bootstrap/js/bootstrap.min.js")!!}'></script>
 <script type='text/javascript' src='{!! asset("/js/main.js")!!}'></script>
+
+<script>
+$( document ).ready(function() {
+
+
+    $('.hiddenUntilAttending').hide();
+    $('.hiddenUntilChild').hide();
+    $('.hiddenUntilNotChild').hide();
+    $('.hiddenUntilNotDirectInvitation').hide();
+
+    $('input[name="attending"]').click(function(eventObj){
+        var obj = eventObj.target;
+
+        if(obj.id === 'attending1'){
+            $('.hiddenUntilAttending').show();
+        } else {
+            $('.hiddenUntilAttending').hide();
+        }
+    });
+
+    $('input[name="isKid"]').click(function(eventObj){
+        var obj = eventObj.target;
+
+        if(obj.id === 'isKid1'){
+            $('.hiddenUntilChild').show();
+            $('.hiddenUntilNotChild').hide();
+        } else {
+            $('.hiddenUntilChild').hide();
+            $('.hiddenUntilNotChild').show();
+        }
+    });
+
+    $('input[name="receivedDirectInvitation"]').click(function(eventObj){
+        var obj = eventObj.target;
+
+        if(obj.id === 'receivedDirectInvitation1'){
+            $('.hiddenUntilNotDirectInvitation').hide();
+        } else {
+            $('.hiddenUntilNotDirectInvitation').show();
+        }
+    });
+});
+</script>
+
+
 </body>
 
 
