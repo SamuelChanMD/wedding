8e01568aad6c8d8c841e8ae92a13184fc716abcc
SamuelChanMD <samuelchanmd@gmail.com>
2017-12-26 11:09:41 -0500

    Sam: Draft of actual content for 'our story'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,9 +28,53 @@
             <div class='panel-body text-center' id='our-story-section'>
                 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                     <div class="panel-body">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,
-                        </p>
+
+                    <br>
+                    <br>
+                    <p>We knew of each other a year before we first met. That’s right, it took a whole year before we would even speak to each other! It all started at an IVCF (a Christian club on campus) party. We never actually talked to each other that night but Sam's friends, Joshua and Jordyn, had met her and instantly saw the chemistry that could be. Don’t ask us how, they just did! After the party, they sat Sam down and told him of Sarah and how we were on the same wavelength. Sam shrugged itoff at the time and that was that.</p>
+
+                    <p>Our fates clashed again at a week long camp retreat. Like our previous "encounter", we never talked. Take a look at this picture to see just how close we got without even interacting with each other.</p>
+
+                    <br>
+                    <br>
+                    <div class='col-md-8 col-md-offset-2'>
+                        <img class='img-responsive' src='{!!asset( "images/sarah_sam_mark_central.jpg")!!}' alt='image of Sarah and Sam at Mark Central'>
+                    </div>
+                    <div class='clearfix'></div>                  
+                    <br>
+                    <br>
+                    
+                    <p>Finally, a few months later, we became bible study co-leaders. That’s when this happened</p>
+
+                    <br>
+                    <br>
+                    <div class='col-md-8 col-md-offset-2'>
+                        <img class='img-responive' src='{!!asset( "images/sarah_post.png")!!}' alt='image of Sarah facebook post'>
+                    </div>
+                    <div class='clearfix'></div>
+                    <br>
+                    <br>
+
+                    <p>From "being around but never interacting", we quickly became ingrained into each other's lives as we led Bible studies together and sparred on Saturday mornings. Our lives mixed like gravy and mashed potatoes.</p>
+
+                    <p>Samuel quickly grew feelings for Sarah and had concluded that Sarah felt the same way; in his opinion, ther signs were too many. When he first approached her about this, she grew quiet and then mumbled, "umm, this is awkward". The stars were apparantly not aligned.</p>
+
+                    <p>Despite the awkward encounter, our friendship continued to grow deeper. Samuel had become really confused at this point. He had expected to Sarah to push back on the friendship to confirm her platonic feelings. Pondering on it, he realized that maybe Sarah liked him but was going through some stuff.</p>
+
+                    <p>So, we met up again. Samuel explained that he saw this going two ways. One she liked him but there were things going in her life that prevented her from being in a relationship. Two, she really did not have any interest in a romantic relationship and he really should just forget about all of this.</p>
+
+                    <p>You probably won’t be able to guess what she said. Ready for it?</p>
+
+                    <p>. . .</p>
+
+                    <p>“I guess I could like you if I forced myself to.”</p>
+
+                    <p>Sarah, reluctant to turn Sam down again, had tried to express it more elegantly than last time. Needless to say, Sarah eventually began to reciprocate the feelings a month later and after a couple more months of careful thought, she decided to tell Sam.</p>
+
+                    <p>Our relationship has been more or less followed the same template: Samuel has always been eager to move to the next stage (sometimes impatiently) while Sarah has been cautious (sometimes reluctant). The exception to this ocurred in August of 2017 when Sarah decided she was ready for marriage and rather than telling Samuel (which is what he was waiting for), she decided to keep her feelings hidden from him until they took a trip to BC with youth from OCBC and Joe, another youth leader. On top of Sunny Peaks mountain, Sarah proposed.</p> 
+
+                    <p>So here we are now, getting ready for marriage and we want you to be a part of it 🙂</p>
+
                     </div>
                 </div>
             </div>
