c350398f215d95c9f0b68e08ebf0039e3ea62ddd
Paul Loh <ploh007@uottawa.ca>
2017-12-30 14:35:25 -0500

    Added WOW.js for transitions and minor CSS cleanup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="row">
-        <div role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" id="our-story-btn" aria-controls="collapseOne">
+        <div role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" id="our-story-btn" aria-controls="collapseOne" class="wow fadeInDown" data-wow-offset="200">
             <h2 class="our-story-heading-text">
                 <span id="our-heading-text">Our</span><span id="story-heading-text"> Story</span>
             </h2>
@@ -35,78 +35,73 @@
         </div>
     </div>
 </div>
-<div class='jumbotron' id='third-body'>
+<div class='jumbotron wow slideInUp' id='third-body' data-wow-offset="300">
     <div class='container text-center' id='details-section'>
-        <h1>Details</h1>
+        <h1>Wedding Details</h1>
         <h2>10th June 2018</h2>
         <h2>StoneFields</h2>
         <h2>1985 9th Line Carleton Place  ON K7C 3P2</h2>
-        <h2>Ceremony at 330 PM</h2>
-        <h2>Reception at 600 PM</h2>
+        <h2>Ceremony at 330 PM Reception at 600 PM</h2>
+        <!-- <h2></h2> -->
         <button type="button" class="btn btn-lg" id="rsvp-button" data-toggle="modal" data-target="#rsvpModal">
-            RVSP
+            RSVP
         </button>
     </div>
 </div>
-<div class='jumbotron' id='fourth-body'>
+<div class='jumbotron wow fadeInUp' id='fourth-body' data-wow-offset="300">
     <div class='container text-center'>
-        <h1>Todo in Ottawa</h1>
+        <h1>Things Todo in Ottawa</h1>
+        <p>If you're coming to Ottawa for the first time, we've compiled a list of things that you should do while you're here!</p>
         <div class='col-md-10 col-md-offset-1'>
-        <div id='carousel-section' class='carousel slide' data-ride='carousel'>
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-
-
-            <!-- Wrapper for slides -->
-            <!--Actual content to be added, removed, or modified-->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src='{!!asset( "images/byward_market.jpg")!!}' alt='image of byward market'>
-                    <div class="carousel-caption">
-                        <h2>Byward Market</h2>
-                        <p>Street performers. Lively public. Lots of places to eat.</p>
+            <div id='carousel-section' class='carousel slide' data-ride='carousel'>
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-section" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-section" data-slide-to="1"></li>
+                    <li data-target="#carousel-section" data-slide-to="2"></li>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src='{!!asset( "images/byward_market.jpg")!!}' alt='image of byward market'>
+                        <div class="carousel-caption">
+                            <h2>Byward Market</h2>
+                            <p>Street performers. Lively public. Lots of places to eat.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src='{!!asset( "images/dows_lake.jpg")!!}' alt='image of dows lake'>
+                        <div class="carousel-caption">
+                            <h2>Dows Lake Pavillion</h2>
+                            <p>Take a stroll around Dows Lake and along the Rideau Canal.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src='{!!asset( "images/uji_cafe.jpg")!!}' alt='image of cafe uji'>
+                        <div class="carousel-caption">
+                            <h2>Cafe Uji</h2>
+                            <p>Try some Japanese cheesecake.</p>
+                        </div>
                     </div>
                 </div>
-
-                <div class="item">
-                    <img src='{!!asset( "images/dows_lake.jpg")!!}' alt='image of dows lake'>
-                    <div class="carousel-caption">
-                        <h2>Dows Lake Pavillion</h2>                    
-                        <p>Take a stroll around Dows Lake and along the Rideau Canal.</p>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <img src='{!!asset( "images/uji_cafe.jpg")!!}' alt='image of cafe uji'>
-                    <div class="carousel-caption">
-                        <h2>Cafe Uji</h2>                    
-                        <p>Try some Japanese cheesecake.</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Carousel Control -->
-            <a class='left carousel-control' href='#carousel-section' role='button' data-slide='prev'>
+                <!-- Carousel Control -->
+                <a class='left carousel-control' href='#carousel-section' role='button' data-slide='prev'>
                 <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
                 <span class='sr-only'>Previous</span>
             </a>
-            <a class='right carousel-control' href='#carousel-section' role='button' data-slide='next'>
+                <a class='right carousel-control' href='#carousel-section' role='button' data-slide='next'>
                 <span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
                 <span class='sr-only'>Next</span>
             </a>
-        </div>
+            </div>
         </div>
     </div>
 </div>
-<div class='jumbotron' id='fifth-body'>
+<div class='jumbotron' id='fifth-body' data-wow-offset="300">
     <div class='container'>
         <div class='col-md-12 text-center'>
             <h1>Wedding Party</h1>
-            <div class='col-md-6'>
+            <div class='col-md-6 wow fadeInLeft' data-wow-duration="1.25s">
                 <h2>Girls</h2>
                 <h3 class='huge'>Grace Kim</h3>
                 <div class='wedding-image-holder'>
@@ -115,7 +110,7 @@
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
             </div>
-            <div class='col-md-6'>
+            <div class='col-md-6 wow fadeInRight' data-wow-duration="1.25s">
                 <h2>Guys</h2>
                 <h3 id='guy_name' class='huge'>Paul Loh</h3>
                 <div id='imagesOfGuys' class='wedding-image-holder'>
@@ -129,7 +124,7 @@
         </div>
     </div>
     <div class='container'>
-<!--         <div class='col-md-12'>
+        <!--         <div class='col-md-12'>
             <ul id='list-girls' class='wedding-party-list'>
                 <li><a href='#girl_name' name='girl' value='grace_kim'>Grace<br>Kim</a></li>
                 <li><a href='#girl_name' name='girl' value='chelsea_anne'>Chelsea<br>Anne</a></li>
@@ -190,6 +185,8 @@
     </div>
 </div>
 <div class='container text-center'>
-    <footer> Sam and Sarah 2017 </footer>
+    <footer>
+        <h3>Sam and Sarah 2017</h3>
+    </footer>
 </div>
 @component('modal') @endcomponent @stop
