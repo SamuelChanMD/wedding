0d5d1580eff533f067468d668fe5bd8ada7770cd
Paul Loh <ploh007@uottawa.ca>
2018-01-06 16:37:46 -0500

    Added Menubar, ScrollBar

--- public/js/main.js
+++ public/js/main.js
@@ -1,92 +1,131 @@
 $(document).ready(function() {
 
-	var boys = {
-		'paul_loh': {
-			'name': 'Paul Loh',
-			'description': 'aeefase asefesaf asefea esf g boij ijl.'
-		},
-		'paul_beckmann': {
-			'name': 'Paul Beckmann',
-			'description': 'bxbae saefesf asefea esf g boij iesafasefjl.'
-		},
-		'aaron_wong': {
-			'name': 'Aaron Wong',
-			'description': 'aeefase asefesaf.'
-		},
-		'daniel_chan': {
-			'name': 'Daniel Chan',
-			'description': 'Daniel is my second brother. yoyo.aeefase asefesaf asefea esf g boij ijl.'
-		},
-		'ezekiel_chan': {
-			'name': 'Jedi Master Ezekiel Chan',
-			'description': 'At a young age, he became permanently blind from a fire. Now he travels around the world, searching for revenge.'
-		},
-		'jonathan_chan': {
-			'name': 'Jonathan Chan',
-			'description': 'Jonathan sings like Pavarotti aeefase asefesaf asefea esf g boij ijl.'
-		}
-	};
+    var boys = {
+        'paul_loh': {
+            'name': 'Paul Loh',
+            'description': 'aeefase asefesaf asefea esf g boij ijl.'
+        },
+        'paul_beckmann': {
+            'name': 'Paul Beckmann',
+            'description': 'bxbae saefesf asefea esf g boij iesafasefjl.'
+        },
+        'aaron_wong': {
+            'name': 'Aaron Wong',
+            'description': 'aeefase asefesaf.'
+        },
+        'daniel_chan': {
+            'name': 'Daniel Chan',
+            'description': 'Daniel is my second brother. yoyo.aeefase asefesaf asefea esf g boij ijl.'
+        },
+        'ezekiel_chan': {
+            'name': 'Jedi Master Ezekiel Chan',
+            'description': 'At a young age, he became permanently blind from a fire. Now he travels around the world, searching for revenge.'
+        },
+        'jonathan_chan': {
+            'name': 'Jonathan Chan',
+            'description': 'Jonathan sings like Pavarotti aeefase asefesaf asefea esf g boij ijl.'
+        }
+    };
 
-	var girls = {
-		'grace_kim': {
-			'name': 'Grace Kim',
-			'description': 'aeefase asefesaf asefea esf g boij ijl.'
-		},
-		'puja_ahluwalia': {
-			'name': 'Puja Ahluwali',
-			'description': 'bxbae saefesf asefea esf g boij iesafasefjl.'
-		},
-		'chelsea_anne': {
-			'name': 'Chelsea Anne',
-			'description': 'aeefase asefesaf.'
-		},
-		'jessica_lee': {
-			'name': 'Jessica Lee',
-			'description': 'Daniel is my second brother. yoyo.aeefase asefesaf asefea esf g boij ijl.'
-		},
-		'vikki_moon': {
-			'name': 'Vikki Moon',
-			'description': 'At a young age, he became permanently blind from a fire. Now he travels around the world, searching for revenge.'
-		},
-		'randelee_macdonald': {
-			'name': 'Randelee MacDonald',
-			'description': 'Jonathan sings like Pavarotti aeefase asefesaf asefea esf g boij ijl.'
-		}
-	};
+    var girls = {
+        'grace_kim': {
+            'name': 'Grace Kim',
+            'description': 'aeefase asefesaf asefea esf g boij ijl.'
+        },
+        'puja_ahluwalia': {
+            'name': 'Puja Ahluwali',
+            'description': 'bxbae saefesf asefea esf g boij iesafasefjl.'
+        },
+        'chelsea_anne': {
+            'name': 'Chelsea Anne',
+            'description': 'aeefase asefesaf.'
+        },
+        'jessica_lee': {
+            'name': 'Jessica Lee',
+            'description': 'Daniel is my second brother. yoyo.aeefase asefesaf asefea esf g boij ijl.'
+        },
+        'vikki_moon': {
+            'name': 'Vikki Moon',
+            'description': 'At a young age, he became permanently blind from a fire. Now he travels around the world, searching for revenge.'
+        },
+        'randelee_macdonald': {
+            'name': 'Randelee MacDonald',
+            'description': 'Jonathan sings like Pavarotti aeefase asefesaf asefea esf g boij ijl.'
+        }
+    };
 
-	/* should be able to refactor these two functions into one
-	 * or at least combine most of their logic into another funciton
-	 */
-	$('a[name="guy"]').click(function(eventObj){
-		var name_key = $(this).attr('value');
-		var img_id = '#' + name_key + '_img';
+    /* should be able to refactor these two functions into one
+     * or at least combine most of their logic into another funciton
+     */
+    $('a[name="guy"]').click(function(eventObj) {
+        var name_key = $(this).attr('value');
+        var img_id = '#' + name_key + '_img';
 
-		$('#guy_name').html(boys[name_key]['name']);
-		$('#guy_description').html(boys[name_key]['description']);
-		$('#imagesOfGuys').children().removeClass('current-guy');
-		$(img_id).addClass('current-guy');
-	});
+        $('#guy_name').html(boys[name_key]['name']);
+        $('#guy_description').html(boys[name_key]['description']);
+        $('#imagesOfGuys').children().removeClass('current-guy');
+        $(img_id).addClass('current-guy');
+    });
 
-	$('a[name="girl"]').click(function(eventObj){
-		var name_key = $(this).attr('value');
-		var img_id = '#' + name_key + '_img';
+    $('a[name="girl"]').click(function(eventObj) {
+        var name_key = $(this).attr('value');
+        var img_id = '#' + name_key + '_img';
 
-		$('#girl_name').html(girls[name_key]['name']);
-		$('#girl_description').html(girls[name_key]['description']);
-		$('#imagesOfGirls').children().removeClass('current-girl');
-		$(img_id).addClass('current-girl');
-	});
+        $('#girl_name').html(girls[name_key]['name']);
+        $('#girl_description').html(girls[name_key]['description']);
+        $('#imagesOfGirls').children().removeClass('current-girl');
+        $(img_id).addClass('current-girl');
+    });
 
     $('.carousel').carousel({
-       interval: false
+        interval: false
     })
 
     $('.our-story-btn-svg').click(
-	    function(){
-	    	$('.our-story-btn-svg').toggleClass('rotate-anim');
-	    	$('#our-heading-text').toggleClass('our-highlight-text');
-	    	$('#story-heading-text').toggleClass('story-highlight-text');
-		}
-	);
+        function() {
+            $('.our-story-btn-svg').toggleClass('rotate-anim');
+            $('#our-heading-text').toggleClass('our-highlight-text');
+            $('#story-heading-text').toggleClass('story-highlight-text');
+        }
+    );
 
+    $("#wedding-nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#third-body").offset().top
+        }, 2000);
+    });
+
+    $("#things-nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#fourth-body").offset().top
+        }, 2000);
+    });
+
+    $("#wedding-party-nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#fifth-body").offset().top
+        }, 2000);
+    });
+
+    $("#registry-nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#sixth-body").offset().top
+        }, 2000);
+    });
+
+    $("#scrollTopBtn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("html,body").offset().top
+        }, 2000);
+    });
 });
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollTopBtn").style.display = "none";
+    }
+}
